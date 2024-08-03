3ab00c756f702358510846d05d52a2ece70a0171
Onkar-Naik <on23337@gmail.com>
2024-08-04 03:27:40 +0530

    SOLVED EASY HACKERANK PYTHON PROBLEMS AND UPDATED git_commands FOR git add COMMAND

---
+++ python/hackerrank/4_python_division.py
@@ -0,0 +1,13 @@
+"""
+The provided code stub reads two integers,  and , from STDIN.
+
+Add logic to print two lines. The first line should contain the result of integer division, a // b. The second line should contain the result of float division, a / b.
+
+No rounding or formatting is necessary.
+"""
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b) #floor division returns int
+    print(a/b) #Returns float
+
