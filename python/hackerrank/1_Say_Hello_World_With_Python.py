aade9a6d48b2cff3b155fe3eaa0151fe003e5c34
Onkar-Naik <on23337@gmail.com>
2024-08-04 02:38:20 +0530

    CREATED A FOLDER UNDER PYTHON : LITCODE AND HACKERRANK AS WELL AS ADDED FIRST PYTHON PROGRAM IN HACKERRANK

---
+++ python/hackerrank/1_Say_Hello_World_With_Python.py
@@ -0,0 +1,20 @@
+"""
+Input Format
+
+You do not need to read any input in this challenge.
+
+Output Format
+
+Print Hello, World! to stdout.
+
+Sample Output 0
+
+Hello, World!
+"""
+#print("Hello, World!")
+""" """
+#We can print message by another way as well by saving message in the variable and print it
+a = "Hello, World!"
+print(a)
+
+""" """
